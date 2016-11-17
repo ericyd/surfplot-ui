b8e747908c53b9ca784bd67d19b8980bda3d3988
Eric Dauenhauer <eric@ericyd.com>
2016-11-16 22:02:08 -0800

    refactor header into new component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+'use strict';
+
 import React, { Component } from 'react';
-import './App.css';
+import Header from './header/Header';
+import './App.scss';
+
 
 class App extends Component {
     render () {
         return (
             <div className="App">
-                <div className="App-header">
-                    <img src="img/logo.svg" className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                </div>
+                <Header />
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
