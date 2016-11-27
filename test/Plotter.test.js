59bb034336a374acd79abdafec5b29f43858051e
Eric Dauenhauer <eric@ericyd.com>
2016-11-26 19:01:49 -0800

    add test cases for edit and delete equations

--- test/Plotter.test.js
+++ test/Plotter.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { should } from 'chai';
 import Plotter from '../src/views/plotter/Plotter';
 
@@ -28,5 +28,37 @@ describe('<Plotter />', function () {
         buttons.at(1).simulate('click', { target: { name: buttons.at(1).prop('name') } });
         wrapper.state('isEquationBarCollapsed').should.equal(true);
         wrapper.state('isOptionBarCollapsed').should.equal(true);
+    });
+
+    it('should always have at least one equation', () => {
+        const wrapper = mount(<Plotter handleUnmount={(e) => e} initialState={{}} />);
+        wrapper.find('.equation__delete').at(0).simulate('click');
+        wrapper.find('.equation').should.have.length(1);
+    });
+
+    it('should add equations when the `+` button is clicked', () => {
+        const wrapper = mount(<Plotter handleUnmount={(e) => e} initialState={{}} />);
+        wrapper.find('.equation').should.have.length(1);
+        wrapper.find('.addEquation').at(0).simulate('click');
+        wrapper.find('.equation').should.have.length(2);
+        wrapper.find('.addEquation').at(0).simulate('click');
+        wrapper.find('.addEquation').at(0).simulate('click');
+        wrapper.find('.equation').should.have.length(4);
+    });
+
+    it('should save values when equation inputs are changed', () => {
+        const wrapper = mount(<Plotter handleUnmount={(e) => e} initialState={{}} />);
+        wrapper.find('.equation__input').at(0).simulate('change', { target: { id: 1, value: 'this' } });
+        wrapper.find('.equation__input').at(0).prop('value').should.equal('this');
+    });
+
+    it('should should be able to remove equations with the `-` button', () => {
+        const wrapper = mount(<Plotter handleUnmount={(e) => e} initialState={{}} />);
+        wrapper.find('.addEquation').at(0).simulate('click'); // 2
+        wrapper.find('.addEquation').at(0).simulate('click'); // 3
+        wrapper.find('.equation__delete').at(0).simulate('click'); // 2
+        wrapper.find('.equation').should.have.length(2);
+        wrapper.find('.equation__delete').at(0).simulate('click'); // 1
+        wrapper.find('.equation').should.have.length(1);
     });
 });
