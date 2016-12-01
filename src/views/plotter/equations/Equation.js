c2e63c4aec49042d0959fbad555cbced5f4f4b10
Eric Dauenhauer <eric@ericyd.com>
2016-11-30 22:19:47 -0800

    Update event handling for onChange and onKeyUp
    Add a timeout so that the parent state is only updated once the equation is no longer being edited and it is parse-able.

--- src/views/plotter/equations/Equation.js
+++ src/views/plotter/equations/Equation.js
@@ -1,17 +1,46 @@
 'use strict';
 
 import React, { Component } from 'react';
+import mathjs from 'mathjs';
 import './equation.scss';
 
 export default class Equation extends Component {
     constructor () {
         super();
         this.handleChange = this.handleChange.bind(this);
+        this.handleKeyUp = this.handleKeyUp.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
+        this.timeout = false;
+        this.state = {
+            value: ''
+        };
+    }
+
+    componentWillMount () {
+        this.setState({ value: this.props.value });
+    }
+
+    handleKeyUp (e) {
+        // required due to React's synthetic events
+        e.persist();
+
+        if (this.timeout) {
+            clearTimeout(this.timeout);
+        }
+
+        // set a timeout so that the function can be edited without sending multiple updates
+        this.timeout = setTimeout(() => {
+            if (e.target.value !== this.state.value && mathjs.eval(e.target.value, { x: 0, y: 0 })) {
+                // if a new and parse-able value, send back up to Plotter.js
+                this.props.handleChange(parseInt(e.target.id, 10), e.target.value);
+            } else {
+                console.log('couldnt parse ', e.target.value, 'or the values didn\'t change');
+            }
+        }, 2000);
     }
 
     handleChange (e) {
-        this.props.handleChange(parseInt(e.target.id, 10), e.target.value);
+        this.setState({ value: e.target.value });
     }
 
     handleDelete (e) {
@@ -22,8 +51,9 @@ export default class Equation extends Component {
         return (
             <div className='equation'>
                 <input type='text'
-                    value={this.props.value}
+                    value={this.state.value}
                     id={this.props.id}
+                    onKeyUp={this.handleKeyUp}
                     onChange={this.handleChange}
                     className='equation__input' />
                 <button type='button'
