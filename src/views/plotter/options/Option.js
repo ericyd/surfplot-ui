9e114ea15cfc5d6462735b6505fe8741f47c3c88
Eric Dauenhauer <eric@ericyd.com>
2016-12-07 21:12:08 -0800

    Update render and handleChange functions to handle different cases
    Options can be arrays (ranges), booleans, or strings (selects), and each has a slightly different way of rendering and updating.

--- src/views/plotter/options/Option.js
+++ src/views/plotter/options/Option.js
@@ -4,12 +4,20 @@ import React, { Component } from 'react';
 import './option.scss';
 import '../../../icon.scss';
 
+/**
+ * Option will render one of three types of inputs:
+ * 1. A component with two text inputs representing a range of values (if this.props.value is an array)
+ * 2. A checkbox (if this.props.value is a boolean)
+ * 3. A select (if this.props.value is a string) with this.props.values as <option>s
+ */
+
 export default class Option extends Component {
     constructor () {
         super();
         this.state = {};
         this.handleChange = this.handleChange.bind(this);
         this.handleKeyUp = this.handleKeyUp.bind(this);
+        this.renderOption = this.renderOption.bind(this);
     }
 
     componentWillMount () {
@@ -36,47 +44,81 @@ export default class Option extends Component {
 
     // update internal state to allow user interaction
     handleChange (e) {
+        if (Array.isArray(this.state.value)) {
+            // create a copy of the value, and update the relevant value
+            const newValue = this.state.value.slice();
+            newValue[e.target.dataset.isMax] = e.target.value;
+            this.setState({ value: newValue });
+
+            // Updates for ranges are validated before calling passed in functions.
+            // Booleans and selects don't need validation, so they can be updated immediately
+            return;
+        }
         this.setState({ value: e.target.value });
+        this.props.handleChange(e.target.id, e.target.value);
+    }
+
+    renderOption (value) {
+        if (Array.isArray(value)) {
+            return (
+                <span>
+                    {this.props.name}
+                    <label htmlFor={this.props.id + 'min'}>min
+                    <input type='text'
+                        value={this.state.value[0]}
+                        data-isMax={0}
+                        id={this.props.id + 'min'}
+                        onChange={this.handleChange}
+                        onKeyUp={this.handleKeyUp} />
+                    </label>
+                    <label htmlFor={this.props.id + 'max'}>max
+                    <input type='text'
+                        value={this.state.value[1]}
+                        data-isMax={1}
+                        id={this.props.id + 'max'}
+                        onChange={this.handleChange}
+                        onKeyUp={this.handleKeyUp} />
+                    </label>
+                </span>
+            );
+        } else if (typeof value === 'boolean') {
+            return (
+                <label htmlFor={this.props.id}>{this.props.name}
+                <input type='text'
+                    value={this.state.value}
+                    id={this.props.id}
+                    onChange={this.handleChange} />
+                </label>
+            );
+        } else {
+            return (
+                <span>
+                    {this.props.name}
+                    <select onChange={this.handleChange}
+                        id={this.props.id}
+                        value={this.state.value}>
+                        {
+                            this.props.values.map((value) => {
+                                return (
+                                    <option key={value}
+                                        value={value}>
+                                        {value}
+                                    </option>
+                                );
+                            })
+                        }
+                    </select>
+                </span>
+            );
+        }
     }
 
     render () {
         return (
             <div className='option'>
-                {this.props.name}
-
-                {this.props.values ? (
-                    // select
-                    <span>select</span>
-                ) : (
-                    // input
-                    <label htmlFor={this.props.id}>{this.props.name}
-                    <input type='text'
-                        value={this.state.value}
-                        id={this.props.id}
-                        onChange={this.handleChange}
-                        onKeyUp={this.handleKeyUp} />
-                    </label>
-                )}
+                {this.renderOption(this.props.value)}
             </div>
         );
-        // return (
-        //     <span>
-        //         <label htmlFor='min'>Min:
-        //         <input type='text'
-        //             value={this.state.min}
-        //             id='min'
-        //             data-index={this.props.index}
-        //             onChange={this.handleChange} />
-        //         </label>
-        //         <label htmlFor='max'>Max:
-        //         <input type='text'
-        //             value={this.state.max}
-        //             id='max'
-        //             data-index={this.props.index}
-        //             onChange={this.handleChange} />
-        //         </label>
-        //     </span>
-        // );
     }
 }
 
@@ -86,7 +128,7 @@ Option.propTypes = {
     parent: React.PropTypes.string,
     value: React.PropTypes.oneOfType([
         React.PropTypes.string,
-        React.PropTypes.number,
+        React.PropTypes.array,
         React.PropTypes.bool
     ])
 };
