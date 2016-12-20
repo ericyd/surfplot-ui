b124a05713e0182fd2eeac527e8e7fb0ae06e840
Eric Dauenhauer <eric@ericyd.com>
2016-12-19 20:06:37 -0800

    Fix Equation handleChange to support non-numeric ids

--- src/views/plotter/equations/Equation.js
+++ src/views/plotter/equations/Equation.js
@@ -33,7 +33,7 @@ export default class Equation extends Component {
             if (e.target.value !== this.props.value) {
                 if (mathjs.eval(e.target.value, { x: 0, y: 0 })) {
                     // if a new and parse-able value, send back up to Plotter.js
-                    this.props.handleChange(parseInt(e.target.id, 10), e.target.value);
+                    this.props.handleChange(e.target.id, e.target.value);
                 } else {
                     console.log('couldnt parse ', e.target.value);
                 }
