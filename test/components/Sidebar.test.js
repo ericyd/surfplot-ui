fc284c4a69eddd2dc65cf28daa49a27416e48606
Eric Dauenhauer <eric@ericyd.com>
2016-12-27 19:06:50 -0800

    Fix Sidebar tests
    Sidebar was refactored and consolidated from EquationBar and OptionsBar and has a radically different construction and purpose.

--- test/components/Sidebar.test.js
+++ test/components/Sidebar.test.js
@@ -4,60 +4,59 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import { should } from 'chai';
 import Sidebar from '../../src/components/Sidebar';
+import Plotter from '../../src/views/Plotter';
+import getGroups from '../../src/utilities/getGroups';
+import ValidatedInput from '../../src/components/ValidatedInput';
+import DirectInput from '../../src/components/DirectInput';
 
 should();
 
-const equations = [
-    {
-        value: 'cos(x)-sin(y)'
-    }
-];
+const groups = getGroups();
 
 describe('<Sidebar />', function () {
     it('should default to hidden', () => {
-        const wrapper = shallow(<Sidebar isCollapsed equations={equations} />);
+        const wrapper = shallow(<Sidebar
+            isCollapsed
+            groups={groups}
+            side='left' />);
         wrapper.prop('className').indexOf('hide').should.equal(0);
         wrapper.prop('className').indexOf('show').should.equal(-1);
     });
 
     it('should change classes based on isCollapsed property', () => {
-        const wrapper = shallow(<Sidebar isCollapsed={false} equations={equations} />);
+        const wrapper = shallow(<Sidebar
+            isCollapsed={false}
+            groups={groups}
+            side='left' />);
         wrapper.prop('className').indexOf('hide').should.equal(-1);
         wrapper.prop('className').indexOf('show').should.equal(0);
     });
 
-    it('should render one equation <div> for each equation in equations', () => {
-        let wrapper = mount(<Sidebar isCollapsed={false} equations={equations} />);
-        wrapper.find('.equation').should.have.length(1);
+    it('should render the correct number of <ValidatedInput> and <DirectInput>', () => {
+        // get count of validated and direct inputs for comparison
+        let numValidatedInputs = 0;
+        let numDirectInputs = 0;
+        Object.keys(groups).forEach((groupName, i) => {
+            for (let i = 0; i < groups[groupName].items.length; i++) {
+                let item = groups[groupName].items[i];
+                if (item.isValidated) {
+                    numValidatedInputs++;
+                } else {
+                    numDirectInputs++;
+                }
+            }
+        });
 
-        equations.push({ value: 'sin(y)+x' });
-        wrapper = mount(<Sidebar isCollapsed={false} equations={equations} />);
-        wrapper.find('.equation').should.have.length(2);
+        // render Sidebar with all groups and state values
+        const plotter = new Plotter();
+        let wrapper = mount(<Sidebar
+            isCollapsed
+            groups={groups}
+            {...plotter.state}
+            side='left' />);
 
-        equations.unshift({ value: 'cos(x^2 + y^2)' });
-        wrapper = mount(<Sidebar isCollapsed={false} equations={equations} />);
-        wrapper.find('.equation').should.have.length(3);
-
-        equations.pop();
-        wrapper = mount(<Sidebar isCollapsed={false} equations={equations} />);
-        wrapper.find('.equation').should.have.length(2);
-
-        equations.shift();
-        wrapper = mount(<Sidebar isCollapsed={false} equations={equations} />);
-        wrapper.find('.equation').should.have.length(1);
-    });
-});
-
-describe('<Sidebar />', function () {
-    it('should default to hidden', () => {
-        const wrapper = shallow(<Sidebar isCollapsed />);
-        wrapper.prop('className').indexOf('hide').should.equal(0);
-        wrapper.prop('className').indexOf('show').should.equal(-1);
-    });
-
-    it('should change classes based on isCollapsed property', () => {
-        const wrapper = shallow(<Sidebar isCollapsed={false} />);
-        wrapper.prop('className').indexOf('hide').should.equal(-1);
-        wrapper.prop('className').indexOf('show').should.equal(0);
+        // find validated and direct inputs and compare to number counted from getGroups
+        wrapper.find(ValidatedInput).should.have.length(numValidatedInputs);
+        wrapper.find(DirectInput).should.have.length(numDirectInputs);
     });
 });
