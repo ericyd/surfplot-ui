8471fe06a673a8b8f2d8ae5f9f85a9970adcd7bd
Eric Dauenhauer <eric_yd@yahoo.com>
2017-02-08 21:11:08 -0800

    Add spinner, checkmark, and times icons

---
+++ src/icons/Times.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Icon from './Icon';
+
+const Times = props => (
+    <Icon className={props.className}>
+        <path d="M10 10 L90 90 M90 10 L 10 90" />
+    </Icon>
+);
+
+Times.propTypes = {
+    className: React.PropTypes.string
+};
+
+export default Times;
